7427abef7b6f0657c1777cb232913136064711bf
Begona Guereca <begonaguereca@gmail.com>
2017-11-16 14:11:17 -0800

    get API data

--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const path = require('path');
-const yelp = require('../api/yelp.js');
+const yelpAPI = require('../api/yelp.js');
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, function () { console.log('Event-gers app listening on port 3000!') });
@@ -13,6 +13,6 @@ app.use(bodyParser.urlencoded({ extended: false }));
 
 
 app.get('/testYelp', function (req, res) {
-  yelp.getYelpResults()
+  yelpAPI.getYelpResults()
   res.send('GET request to the homepage')
 })
