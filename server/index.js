641ee6f8558516c9cb21c14ca06aac7f48a4238a
Hack Reactor Student <student@hackreactor.com>
2017-11-16 12:40:34 -0800

    changed directory name for statically rendered files in server.js

--- server/index.js
+++ server/index.js
@@ -4,8 +4,11 @@ const bodyParser = require('body-parser');
 const path = require('path');
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, function () { console.log('Event-gers app listening on port 3000!') });
 
 app.use(bodyParser.json());
-app.use(express.static(path.join(__dirname, '../client/dist')));
+app.use(express.static(path.join(__dirname, '../client/')));
 app.use(bodyParser.urlencoded({ extended: false }));
+
+
+app.listen(PORT, function () { console.log('Event-gers app listening on port 3000!') });
+
