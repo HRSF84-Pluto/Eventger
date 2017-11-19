bbaaf1c16bd3d0d0383cadda09d2c7d612dc88e6
briceida <briceida@Eriks-MacBook-Pro-2.local>
2017-11-19 11:19:03 -0800

    Changed webpack settings to run on Heroku

--- webpack.config.js
+++ webpack.config.js
@@ -1,21 +1,21 @@
 module.exports = {
- entry: "./client/src/app.jsx",
- output: {
-   filename: "./client/dist/bundle.js"
- },
- module: {
-   loaders: [
-     {
-       test: [/\.es6$/, /\.jsx?/],
-       exclude: /node_modules/,
-       loader: 'babel-loader',
-       query: {
-         presets: ['react', 'es2015']
-       }
-     }
-   ]
- },
- resolve: {
-   extensions: ['.jsx','.js','.es6']
- },
-}
+  entry: './client/src/app.jsx',
+  output: {
+    filename: './client/dist/bundle.js',
+  },
+  module: {
+    loaders: [
+      {
+        test: [/\.es6$/, /\.jsx?/],
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['react', 'es2015'],
+        },
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.jsx', '.js', '.es6'],
+  },
+};
