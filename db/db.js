b0ebe1c3dc91e5e71486720a17f5112856c522cf
Garrett Hara <ghara537@gmail.com>
2017-11-19 15:20:30 -0800

    Before including postgres into code

--- db/db.js
+++ db/db.js
@@ -1,6 +1,7 @@
 var Sequelize = require('sequelize');
 var mysql = require('mysql');
-var sequelize = new Sequelize('Eventger') {
+var sequelize = new Sequelize({
+  database: 'Eventger',
   username: 'root',
   host: 'localhost',
   dialect: 'mysql',
@@ -11,14 +12,14 @@ var sequelize = new Sequelize('Eventger') {
     acquire: 30000,
     idle: 10000
   },
-}
+});
 
 
-var connection = mysql.connection({
-  host: 'localhost',
-  user: 'root',
-  database: 'Eventger'
-})
+// var connection = mysql.connection({
+//   host: 'localhost',
+//   user: 'root',
+//   database: 'Eventger'
+// })
 
 // connection.connect((error) => {
 //   if
@@ -26,16 +27,98 @@ var connection = mysql.connection({
 
 //Change the password and username -------------------------------------
 
-// sequelize.authenticate()
-//   .then(() => {
-//     console.log('Connection to MySQL Database Successful');
-//   }.catch(error => {
-//     console.log('Error with connection to database ', error)
-//   })
+sequelize.authenticate()
+  .then(() => {
+    console.log('Connection to MySQL Database Successful');
+    
+
+
+    const User = sequelize.define('user', {
+      // id: {
+      //   type: Sequelize.INTEGER,
+      //   autoIncrement: true,
+      //   unique: true,
+      //   primaryKey: true
+      // },
+      displayName: {
+        type: Sequelize.STRING
+      },
+      username: {
+        type: Sequelize.STRING,
+        // unique: true
+      },
+      password: {
+        type: Sequelize.STRING
+      },
+      location: {
+        type: Sequelize.STRING
+      }
+    });
+
+    // force: true will drop the table if it already exists
+    User.sync({force: true}).then(() => {
+      // Table created
+      return User.create({
+        displayName: 'John',
+        username: 'Hancock'
+      });
+    }).catch((err) => {console.log('ERROR', err)});
+
+
+
+
+  }).catch(error => {
+    console.log('Error with connection to database ', error)
+  })
+
+
+
+// const Event = sequelize.define( 'event', {
+//   id: {
+//     type: Sequelize.INTEGER,
+//     autoIncrement: true,
+//     unique: true,
+//     primaryKey: true
+//   },
+//   name: {
+//     type: Sequelize.STRING
+//   },
+//   description: {
+//     type: Sequelize.STRING
+//   },
+//   when: {
+//     type: Sequelize.STRING
+//   },
+//   location: {
+//     type: Sequelize.STRING
+//   },
+//   price: {
+//     type: Sequelize.STRING
+//   },
+//   url: {
+//     type: Sequelize.STRING,
+//     isUrl: true
+//   },
+//   photoUrl: {
+//     type: Sequelize.STRING
+//   },
+//   category: {
+//     type: Sequelize.STRING
+//   }
+
+// })
+
+// Event.sync({force: true}).then(() => {
+//   // Table created
+//   return Event.create({
+//     name: 'Golden State Warriors vs Los Angeles Lakers',
+//     description: 'The most epic game of the season.'
+//   });
+// });
 
 
 // const Eventger = sequelize.define( 'Eventger Profile')
 
 
 
-module.exports = connection;
+module.exports = sequelize;
