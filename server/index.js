f654234334079b80c763ddb43297e6315164f706
Begona Guereca <begonaguereca@gmail.com>
2017-11-19 12:07:47 -0800

    added routes

--- server/index.js
+++ server/index.js
@@ -2,8 +2,9 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const path = require('path');
-const yelp = require('../api/yelp.js');
+const dataAgg = require('../api/dataAggregator.js');
 const PORT = process.env.PORT || 3000;
+
 
 app.listen(PORT, function () { console.log('Event-gers app listening on port 3000!') });
 
@@ -11,9 +12,43 @@ app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, '../client/')));
 app.use(bodyParser.urlencoded({ extended: false }));
 
+//Pulling new data when params change
+app.get('/eventData', function (req, res) {
+  dataAgg.finalResults('ADD PARAMS HERE',function(data) {
+    res.send(data)
+  });
+});
 
-app.get('/testYelp', function (req, res) {
-  yelp.getYelpResults()
-  res.send('GET request to the homepage')
+
+//Add user to DB
+app.post('/signup', function(req, res) {
+  //req.body.userName
+  // check DB if user exisits
+    //If YES, then send back message that says
+    //the user is already signed up
+    //If NO, then add the user to DB
+  //res.send(answer)
 })
 
+
+//Check to see if user is in DB, and return saved results
+app.post('/login', function(req, res) {
+  //req.body.userName
+  // check DB if user exisits
+    //If YES then return saved db results
+    //If NO then return messsage that says
+    //thye are not in our DB
+  //res.send(answer)
+})
+
+
+/////**** this is repetitive******
+// //Return saved Events to users
+// app.post('/savedevents', function(req, res) {
+//   //req.body.userName
+//   //check DB if user exisits
+//     //if YES, then return saved db results
+//     //If NO, then return message taht says that
+//     //they are not in our DB
+//   //res.send(answer)
+// })
