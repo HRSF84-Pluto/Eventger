b00de15f44f480deef09fff5af16c6bbe44b77c8
briceida <briceida@Eriks-MacBook-Pro-2.local>
2017-11-19 22:44:34 -0800

    added front page components: sign-up and login pages, included semantic-ui

--- client/src/app.jsx
+++ client/src/app.jsx
@@ -1,42 +1,57 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import $ from 'jquery';
+import SignUp from './components/SignUp';
+import Login from './components/Login';
+import Main from './components/Main';
+
+
 
 class App extends React.Component {
-  constructor() {
-    super();
-    this.state = { }
+  constructor(props) {
+    super(props);
+    this.state = {
+      view: 'main',
+    };
   }
 
 
-componentDidMount(){
-  $.ajax({
-    type: 'GET',
-    url: '/testYelp',
-    success: (response)=> {
-      console.log('Inside React componentDidMount')
-    },
-    failure: (err)=> {
-      console.err(err)
+  componentDidMount(){
+    $.ajax({
+      type: 'GET',
+      url: '/testYelp',
+      success: (response)=> {
+        console.log('Inside React componentDidMount')
+      },
+      failure: (err)=> {
+        console.err(err)
+      }
+    });
+  }
+  handleClick(view) {
+    this.setState({ view });
+  }
+
+  renderView() {
+    const { view } = this.state;
+    if (view === 'main') {
+      return <Main onClick={currentView => this.handleClick(currentView)} />;
+    } else if (view === 'login') {
+      return <Login onClick={currentView => this.handleClick(currentView)} />;
+    } else if (view === 'signup') {
+      return <SignUp onClick={currentView => this.handleClick(currentView)} />;
     }
-  });
-}
+  }
 
 
   render() {
     return (
-      <div>
-        <h1>Event-gers</h1>
-        <h3>Take back your Saturday night!</h3>
+      <div className="main">
+          {this.renderView()}
       </div>
-    )
+    );
   }
 }
-
-
-
-
-
 
 
 
---
+++ client/src/components/SignUp.jsx
@@ -0,0 +1,38 @@
+import React, {Component} from 'react';
+import { Button, Checkbox, Form } from 'semantic-ui-react';
+
+class SignUp extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+  render() {
+    return (
+      <div className="signUpPage">
+        <div>
+          <h3>Sign Up</h3>
+        </div>
+        <div onClick={()=> this.props.onClick('main')} className="close">
+        </div>
+        <div className="signUpForm">
+        <Form>
+          <Form.Field>
+            <label>Username</label>
+            <input placeholder='username' />
+          </Form.Field>
+          <Form.Field>
+            <label>Password</label>
+            <input type="password" placeholder='password' />
+          </Form.Field>
+          <Form.Field>
+            <Checkbox label='I agree to the Terms and Conditions' />
+          </Form.Field>
+          <Button type='submit'>Submit</Button>
+        </Form>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default SignUp;
