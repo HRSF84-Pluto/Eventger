f8ce94f971ed6d810079462afcd056968f7d5515
briceida <briceida@Eriks-MacBook-Pro-2.local>
2017-11-19 16:10:45 -0800

    changed test script to run mocha, and modified ServerSpect.js file to run mocha test

--- tests/server/ServerSpecs.js
+++ tests/server/ServerSpecs.js
@@ -1,4 +1,20 @@
 const expect = require('chai').expect;
-const request = require('supertest');
+let request = require('supertest');
+const chai = require('chai')
+  , chaiHttp = require('chai-http');
+
+chai.use(chaiHttp);
 
 request = request('http://localhost:3000');
+
+
+describe('server is listening on port 3000', function() {
+  it('succeeds silently!', function() {
+    chai.request('http://localhost:3000')
+      .get('/')
+      .end(function(err, res) {
+        expect(res).to.have.status(200);
+      });
+  }) ;
+  });
+
