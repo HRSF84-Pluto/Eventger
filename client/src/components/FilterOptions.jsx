3ced81d162e1e81a90db62c3687610d9c9e74f6a
briceida <briceida@Eriks-MacBook-Pro-2.local>
2017-11-21 11:44:25 -0800

    removed unnecessary ImageDiv component

--- client/src/components/FilterOptions.jsx
+++ client/src/components/FilterOptions.jsx
@@ -1,27 +1,43 @@
 import React,{Component} from 'react';
 
 
-const FilterOptions = (props)=> (
-  <div>
-    <label className="container"><p>One</p>
-    <input type="checkbox" />
-    <span className="checkmark"></span>
-  </label>
-  <label className="container"><p>Two</p>
-    <input type="checkbox"/>
-      <span className="checkmark"></span>
-  </label>
-  <label className="container"><p>Three</p>
-    <input type="checkbox"/>
-    <span className="checkmark"></span>
-  </label>
-  <label className="container"><p>Four</p>
-    <input type="checkbox"/>
-      <span className="checkmark"></span>
-  </label>
-  </div>
+class FilterOptions extends Component {
+  constructor(props){
+    super(props);
+    this.state = { budget: false,
+      outdoor: false,
+      indoor: false,
+      wifi: false,
+    };
+  }
+  componentDidUpdate() {
+      console.log("this state", this.state);
+      //makes patch request to server after every input: live event filtering!
+  }
 
-);
+  render() {
+    return (
+    <div>
+      <label className="container"><p>Budget</p>
+        <input type="checkbox" onClick={()=> this.setState({budget: !this.state.budget})} />
+        <span className="checkmark"></span>
+      </label>
+      <label className="container"><p>Outdoor</p>
+        <input type="checkbox" onClick={()=> this.setState({outdoor: !this.state.outdoor})} />
+        <span className="checkmark"></span>
+      </label>
+      <label className="container"><p>Indoor</p>
+        <input type="checkbox" onClick={()=> this.setState({indoor: !this.state.indoor})} />
+        <span className="checkmark"></span>
+      </label>
+      <label className="container"><p>Wifi Access</p>
+        <input type="checkbox" onClick={()=> this.setState({wifi: !this.state.wifi})} />
+        <span className="checkmark"></span>
+      </label>
+    </div>
+    )
+  }
+}
 
 
 export default FilterOptions;
