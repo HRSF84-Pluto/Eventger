34d02733ec1fe96d7aceebd5c80662ada9ea3ec3
Begona Guereca <begonaguereca@gmail.com>
2017-11-20 17:10:28 -0800

    Add ajax request to sign up

--- client/src/components/SignUp.jsx
+++ client/src/components/SignUp.jsx
@@ -1,37 +1,56 @@
 import React, {Component} from 'react';
 import { Button, Checkbox, Form } from 'semantic-ui-react';
+import $ from 'jquery';
 
 class SignUp extends Component {
   constructor(props) {
     super(props);
-    this.state = {username: '',
+    this.state = {
+      username: '',
       password: '',
+      location: '',
       errorMessage : '',
     };
   }
   handleUsernameInput(e){
     this.setState({username: e.target.value});
   }
+  handleZipInput(e){
+    this.setState({location: e.target.value});
+  }
   handlePasswordInput(e){
     this.setState({password: e.target.value});
   }
   handleSignUpInput(){
-    //checks for username in db:
-     // if username exists, redirect to login,
-     // if input is incorrent, show error message
-    const data = {"username": this.state.username, "password": this.state.password};
-    console.log(data, 'data object to be sent to db');
+    // var final =
+    // const data = {'username': this.state.username, 'password': this.state.password, 'location': this.state.location};
+    // console.log(data, 'data object to be sent to db');
+    // //checks for username in db:
+    // $.ajax({
+    //   type: 'POST',
+    //   url: '/signup',
+    //   data: data,
+    //   success: (response)=> {
+    //     console.log('returned from SIGNUP POST Request: ', response)
+    //     // if username exists, redirect to login,
+    //     // if input is incorrent, show error message
+    //   },
+    //   failure: (err)=> {
+    //     console.err(err)
+    //   }
+    // });
     //this function returns true if sign up is successful, and returns false if it's not. This returned Boolean will allow
     //the conditional rendering of the views (done inside the  submit <Button/> below)
 
     // ajax call to server, if error : testing below, this should be inside ajax call
-    const error = true;
-    if (error){
-      this.setState({error: 'Username already exists, please try again'});
-      return false;
-    }else{
-      return true;
-    }
+    // const error = true;
+    // if (error){
+    //   this.setState({error: 'Username already exists, please try again'});
+    //   return false;
+    // }else{
+    //   return true;
+    // }
+    return false
 
   }
   render() {
@@ -49,6 +68,10 @@ class SignUp extends Component {
               <input placeholder='username' onChange={e => this.handleUsernameInput(e)} />
             </Form.Field>
             <h4 className='error' >{this.state.error}</h4>
+              <Form.Field>
+                <label>Zip Code</label>
+                <input placeholder='zipcode' onChange={e => this.handleZipInput(e)} />
+              </Form.Field>
             <Form.Field>
               <label>Password</label>
               <input type="password" placeholder='password' onChange={e => this.handlePasswordInput(e)}  />
