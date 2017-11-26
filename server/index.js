ba48da8acce93b690a89a1f0c99f366c0c9a9357
Garrett Hara <ghara537@gmail.com>
2017-11-25 22:22:18 -0800

    Include function to narrow search options and include preferences array for each user

--- server/index.js
+++ server/index.js
@@ -119,21 +119,29 @@ app.get('/eventData', function (req, res) {
   // to test a sample req.body from front-end's get request
   let sampleReqBody = {
     queryTermForTM: ['sports', 'music'], // both query Terms are defined by homepage selection upon landing on site
-    preferenceForMusicOrLeague: 'Rock', // additional keyword given by user in preferences table [max: 1 word] to narrow down sports or music
-    queryTermForYelp: 'food', // default Yelp fetch from homepage
-    preferenceForFoodAndOrSetting: 'Mexican', // additional keyword given by user to narrow down type of food
+    preferenceForMusicOrLeague: ['Rock', 'Pop', 'Country'], // additional keyword given by user in preferences table [max: 1 word] to narrow down sports or music
+    queryTermForYelp: ['food', 'brewery', 'winery', 'hike'], // default Yelp fetch from homepage 
+    // preferenceForFoodAndOrSetting: 'Mexican', // additional keyword given by user to narrow down type of food
     // activity: 'hiking', // if user doesn't want food but wants an activity - yelp category: https://www.yelp.com/developers/documentation/v2/all_category_list
     city: 'San Francisco',
     postalCode: '94104',
     startDateTime: '2017-01-12T18:00:00Z',
     price: '$$',
   }
+  
 
+  db.reduceSearchAsync(sampleReqBody, 1)
+  .then(sampleReqBody => {
+
+  console.log('Reduced Sample Body', sampleReqBody);
   let returnedYelpTMDataObj = {};
 
   // fetch ticketmaster data
-  fetchHelpers.getTMData(sampleReqBody)
-  .then(ticketMasterEventsArr => {
+  return fetchHelpers.getTMData(sampleReqBody)
+  }).catch(err => {
+    console.log('ERROR in reduceSearchAsync', err)
+  }).then(ticketMasterEventsArr => {
+
 
     // include TM event data in the object sent back to front-end //
     returnedYelpTMDataObj.ticketmaster = ticketMasterEventsArr;
