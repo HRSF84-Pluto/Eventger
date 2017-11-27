0b18bb2e8b3dadbd6077617eacfd52a2c09f7cb5
briceida <BriceidaMariscal@gmail.com>
2017-11-27 13:59:52 -0800

    declared 'fs' in webpack file

--- webpack.config.js
+++ webpack.config.js
@@ -7,6 +7,9 @@ module.exports = {
     path: path.resolve(__dirname, './client/dist'),
     publicPath: './client/dist',
   },
+  node: {
+    fs: 'empty'
+  },
   module: {
     loaders: [
       {
