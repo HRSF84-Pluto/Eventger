9c3a139f30ac60d7b5a5d0dc0905cb3a2c8d63f6
sallyyeng <sallyyeng@gmail.com>
2017-11-28 10:19:52 -0800

    Delete apiKeys.js

--- api/apiKeys.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  tm_api_key: 'WwQdJLddZ3A2dvOf0kG8BAZX3oTYc223',
-  tm_consumer_secret: 'NIVzIzwKWnho3jQj',
-  yelp_api_key: 'yA-a4r-cykSbny1slezZvw',
-  yelp_consumer_secret: 'XdqfCqu916JCPOsVIn7xDzu5C8MhHJ4KArJd5nzXCU8LBONjSyHuGqEPW1tNtyzt',
-  token: '6btS_U7RdYm-U9mWZcQGnrvQ_4fewvIG3Poc-B6DcC_cbwHC0Xwf_TQF691zsGK1aDid0z4fXioCsAOfw6xsHRZcJNvXJKDQfcsKgss1c_mhlGfOkT2U24ajEBsWWnYx',
-  token_secret: 'u-KDzdHnpyElCp7-DrrsPPDsFOc',
-};
-
