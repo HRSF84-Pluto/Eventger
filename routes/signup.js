65e1414b11595580987299ab0a1a381612613a84
briceida <BriceidaMariscal@gmail.com>
2018-01-26 00:16:54 -0800

    Fixed issue that caused sign ups to crash server

--- routes/signup.js
+++ routes/signup.js
@@ -2,27 +2,28 @@ const express = require('express');
 const router = express.Router();
 const bcrypt = require('bcrypt');
 const saltRounds = 10;
-const db =  require('../db/db.js');
+const db = require('../db/db.js');
 
 
 //Add user to DB
-router.post('/', function(req, res) {
-  if (req.body.username === 'Login'){
+router.post('/', function (req, res) {
+  if (req.body.username === 'Login') {
     res.status(403).send();
   }
   //bcrypt is a hashing library
   //bcrypt generates salt
-  bcrypt.genSalt(saltRounds, function(err, salt) {
+  bcrypt.genSalt(saltRounds, function (err, salt) {
     //bcrypt generates hash
-    bcrypt.hash(req.body.password, salt, function(err, hash) {
-      if(err){console.log('error in creating hash, inside routes/signup');}
-        req.body.hash = hash;
-        db.saveUsernameAsync(req.body)
-          .then((results) => res.status(201).send(true))
-          .catch((err)=> {
-            console.log(err);
-            res.status(409).send(false)
-          })
+    bcrypt.hash(req.body.password, salt, function (err, hash) {
+      if (err) {
+        console.log('error in creating hash, inside routes/signup');
+      }
+      db.saveUsernameAsync(req.body, hash)
+        .then((results) => res.status(201).send(true))
+        .catch((err) => {
+          console.log(err);
+          res.status(409).send(false)
+        })
     });
   });
 
